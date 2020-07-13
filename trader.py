518efb47a7ec4f7a99245f52f8d439d6311be78e
ngoraymond <21043327+ngoraymond@users.noreply.github.com>
2020-07-12 22:34:27 -0700

    updated trader

--- trader.py
+++ trader.py
@@ -1,18 +1,25 @@
 import alpaca_trade_api as trade
 import config
+import time
 
-endpoint_url = 'https://paper-api.alpaca.markets'
+def main():
+    endpoint_url = 'https://paper-api.alpaca.markets'
 
-api = trade.REST(key_id = config.API_KEY, secret_key = config.SECRET_KEY, 
-            base_url= 'https://paper-api.alpaca.markets', api_version='v2')
+    api = trade.REST(key_id = config.API_KEY, secret_key = config.SECRET_KEY, 
+                base_url= endpoint_url, api_version='v2')
 
-print(api.list_positions())
+    print(api.list_positions())
 
 
-api.submit_order(
-    symbol = 'SPY',
-    qty = '100',
-    time_in_force = 'opg',
-    side = 'buy',
-    type = 'market'
-)
+    api.submit_order(
+        symbol = 'SPY',
+        qty = '100',
+        time_in_force = 'opg',
+        side = 'buy',
+        type = 'market'
+    )
+    
+    
+
+if __name__ == "__main__":
+    main()
