6f699458248e03a19bb9cf60c383c59bc93567cf
ngoraymond <21043327+ngoraymond@users.noreply.github.com>
2020-07-15 23:13:59 -0700

    added loop

--- tradeRedditPost.py
+++ tradeRedditPost.py
@@ -1,10 +1,9 @@
 import praw
 import time
 
-reddit = praw.Reddit('bot')
-subred = reddit.subreddit('College_Prestige')
-
 def tradePost(position, price, buy):
+    reddit = praw.Reddit('bot')
+    subred = reddit.subreddit('Trading_Activity_rn')
     side = "bought" if buy else "sold"
     subred.submit(position+' '+side+' at '+price+' on '+time.ctime(time.time()))
     print('Posted to Reddit!')
--- trader.py
+++ trader.py
@@ -1,6 +1,8 @@
 import alpaca_trade_api as trade
 import config
 import time
+import datetime
+import tradeRedditPost
 
 def main():
     endpoint_url = 'https://paper-api.alpaca.markets'
@@ -10,14 +12,38 @@ def main():
 
     print(api.list_positions())
 
+    while True:
+        #when starting, close all pending programs
+        pending = api.list_orders(status='open')
+        for orders in pending:
+            api.cancel_order(orders.id)
 
-    api.submit_order(
-        symbol = 'SPY',
-        qty = '100',
-        time_in_force = 'opg',
-        side = 'buy',
-        type = 'market'
-    )
+        #get clock
+        clock = api.get_clock()
+        closeTime = clock.next_close.replace(tzinfo=datetime.timezone.utc).timestamp()
+        openTime = clock.openTime.replace(tzinfo=datetime.timezone.utc).timestamp()
+        curTime = clock.timestamp.replace(tzinfo=datetime.timezone.utc).timestamp()
+        timetoClose = closeTime - curTime
+
+        #For starters, sell when open, buy when close
+
+        #when close
+        api.submit_order(
+            symbol = 'SPY',
+            qty='100',
+            time_in_force='cls',
+            side = 'buy',
+            type = 'market'
+        )
+
+        #when open
+        api.submit_order(
+            symbol = 'SPY',
+            qty='100',
+            time_in_force='opg',
+            side = 'buy',
+            type = 'market'
+        )
     
     
 
